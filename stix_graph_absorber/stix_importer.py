caa507e745f2fe33c09706a8427c04138859659f
Sebastien Mitea <seb.mitea@disinformationindex.org>
2022-03-16 15:33:36 +0100

    Added docstrings

--- stix_graph_absorber/stix_importer.py
+++ stix_graph_absorber/stix_importer.py
@@ -1,31 +1,39 @@
-class Clumper:
+class Graph_absorber:
     def __init__(self, blob) -> None:
+        """Returns list of dictionaries"""
         self.blob=blob
 
     def keep(self, *funcs):
+        """Returns elements that match all functions in *funcs"""
         data = self.blob
         for func in funcs:
             data = [d for d in data if func(d)]
-        return Clumper(data)
+        return Graph_absorber(data)
 
     def head(self, n):
-        return Clumper([self.blob[i] for i in range(n)])
+        """Returns n first elements"""
+        return Graph_absorber([self.blob[i] for i in range(n)])
 
     def tail(self, n):
-        return Clumper([self.blob[-i] for i in range(1, n+1)])
+        """Returns n last elements"""
+        return Graph_absorber([self.blob[-i] for i in range(1, n+1)])
 
     def select(self, *keys):
-        return Clumper([{k: d[k] for k in keys} for d in self.blob])
+        """Returns only elements with key in *keys"""
+        return Graph_absorber([{k: d[k] for k in keys} for d in self.blob])
 
     def mutate(self, **kwargs):
+        """Edit the value of key by applying func, for all key: func pair"""
         data = self.blob
         for key, func in kwargs.items():
             for i in range(len(data)):
                 data[i][key] = func(data[i])
-        return Clumper(data)
+        return Graph_absorber(data)
     
     def sort(self, key, reverse=False):
-        return Clumper(sorted(self.blob, key=key, reverse=reverse))
+        """Sort by key"""
+        return Graph_absorber(sorted(self.blob, key=key, reverse=reverse))
 
     def collect(self):
+        """Return instance of the data object"""
         return self.blob
